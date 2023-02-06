92af9797dd498bedc40e388adb4781fb413eb385
marcia diaz <marciadiazz92@gmail.com>
2023-02-06 19:43:22 -0300

    desafio1

---
+++ desafio.js
@@ -0,0 +1,30 @@
+class ProductManager {
+  products = [];
+  #acumulator = 0;
+
+  //AGREGA PRODUCTOS
+  addProduct(title, description, price, thumbnail, code, stock) {
+    const newProduct = {
+      id: this.#acumulator,
+      title,
+      description,
+      price,
+      thumbnail,
+      code,
+      stock,
+    };
+  }
+
+  //METODO 1 DEVOLVER EL ARREGLO CON TODOS LOS PRODUCTOS
+  getProducts() {
+    return this.products;
+  }
+
+  //METODO2 BUSCAR EL QUE COINCIDA CON EL ID
+  getProductById() {
+    //si no coincide "not found"
+  }
+}
+
+const manager = new ProductManager();
+console.log(manager.getProducts());
